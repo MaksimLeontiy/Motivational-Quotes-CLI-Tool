2f2d8e61b88bd272dc7a39e9cd6004181fed783a
Maksim Leontiy <leontiy.maksim1@gmail.com>
2024-11-13 20:58:10 -0600

    Initial commit, set up motivational quotes cli tool

---
+++ index.js
@@ -0,0 +1,20 @@
+// Initialize axios
+const axios = require('axios')
+// Function to get random quote
+async function getQuote() {
+
+    try {
+        // Request data from api endpoint
+        const response = await axios.get('https://zenquotes.io/api/rando')
+
+        console.log("Here is a motivational quote for you:")
+        // log the api response data
+        console.log(`"${response.data[0].q}" - ${response.data[0].a}`)
+    }
+    // Display error message if api call fails
+    catch {
+        console.log("Could not fetch a quote. Tryagain later.")
+    }
+}
+// Call the function
+getQuote();
